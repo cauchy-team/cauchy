4bd6b76e0918090d23842af7fed801cdc151d86a
Chris Novick <c.r.novick@gmail.com>
2020-03-23 23:35:20 -0400

    First four bytes of contract data are len

--- cauchy-vm/src/lib.rs
+++ cauchy-vm/src/lib.rs
@@ -17,14 +17,17 @@ pub struct WasmVM {}
 impl WasmVM {
     pub fn initialize(_transaction: Bytes) -> Result<(), ()> {
         // TODO: get data from transaction
-        let contract_data = Some(vec![0x41, 0x42, 0x43, 0x44, 0x45]);
+        let contract_data = vec![0x41, 0x42, 0x43, 0x44, 0x45];
+        let mut cdata_builder = contract_data.len().to_le_bytes().to_vec();
+        cdata_builder.extend(contract_data);
+
         let f = std::fs::File::open("contract_data.wasm").unwrap();
         let module = decode_module(BufReader::new(f)).unwrap();
 
         let mut store = init_store();
         let module_instance = instantiate_module(&mut store, module, &[]).unwrap();
         if let ExternVal::Func(main_addr) = get_export(&module_instance, "app_init").unwrap() {
-            let res = invoke_func(&mut store, main_addr, Vec::new(), contract_data);
+            let res = invoke_func(&mut store, main_addr, Vec::new(), Some(cdata_builder));
             println!("{:X?}", res);
             save_store("some_txid", &store);
             Ok(())
@@ -64,8 +67,8 @@ impl CauchyVM for WasmVM {
 
 #[test]
 fn vm_test() {
-    // let res1 = WasmVM::initialize(Bytes::new());
-    // assert!(res1.is_ok());
+    let res1 = WasmVM::initialize(Bytes::new());
+    assert!(res1.is_ok());
     let res2 = WasmVM::process_inbox(Bytes::new(), Bytes::new());
     assert!(res2.is_ok());
 }
