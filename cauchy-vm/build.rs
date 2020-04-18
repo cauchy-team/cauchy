f974aaeea39a99fb536402b3ddf08c0284dfa2a3
Chris Novick <c.r.novick@pm.me>
2020-04-18 11:00:40 -0400

    Cleanup build.rs

--- cauchy-vm/build.rs
+++ cauchy-vm/build.rs
@@ -2,19 +2,12 @@ use std::process::Command;
 
 fn main() {
     let _output = Command::new("cargo")
+        .current_dir("contracts\\contract_data")
         .arg("build")
         .arg("--release")
         .arg("--target=wasm32-unknown-unknown")
-        .current_dir("contracts\\contract_data")
         .output()
         .expect("failed to build contract");
 
-    // Command::new("copy")
-    //     .arg("/y")
-    //     .arg("\"target\\wasm32-unknown-unknown\\release\\contract_data.wasm\"")
-    //     .arg("\"../\"")
-    //     .current_dir("contracts\\contract_data")
-    //     .output()
-    //     .expect("failed to copy contract_data.wasm file :-(");
     println!("cargo:rerun-if-changed=contractscontract_data/src/main.rs");
 }
