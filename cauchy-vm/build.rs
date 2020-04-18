883904bbac6a3c0b153b806bd57879dce195475d
Chris Novick <c.r.novick@pm.me>
2020-04-18 12:24:57 -0400

    Attempt to fix build.rs for tests

--- cauchy-vm/build.rs
+++ cauchy-vm/build.rs
@@ -2,7 +2,7 @@ use std::process::Command;
 
 fn main() {
     let _output = Command::new("cargo")
-        .current_dir("contracts\\contract_data")
+        .current_dir("contracts/contract_data")
         .arg("build")
         .arg("--release")
         .arg("--target=wasm32-unknown-unknown")
