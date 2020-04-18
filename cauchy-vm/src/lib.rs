f294a8fca974121c0496d69e69f0fc0b930ce4ba
Chris Novick <c.r.novick@pm.me>
2020-04-18 12:19:21 -0400

    Refine interface

--- cauchy-vm/src/lib.rs
+++ cauchy-vm/src/lib.rs
@@ -1,18 +1,8 @@
 pub mod merkle;
+mod wasm_vm;
+
 pub use merkle::*;
-
-use std::io::Cursor;
-
-pub fn get_version() -> String {
-    env!("CARGO_PKG_VERSION").to_string()
-}
-
-pub trait CauchyVM {
-    fn initialize(script: &Script<'_>) -> Result<()>;
-    fn process_inbox(script: &Script<'_>, message: Option<Vec<u8>>) -> Result<()>;
-}
-
-use rust_wasm::*;
+pub use wasm_vm::WasmVM as DefaultVM;
 
 pub struct Script<'a> {
     func: Option<&'a str>,
@@ -22,81 +12,44 @@ pub struct Script<'a> {
 
 type Result<T> = std::result::Result<T, VmErr>;
 
+#[derive(Debug)]
 pub enum VmErr {
     Unknown,
 }
 
-pub struct WasmVM {}
-
-impl WasmVM {
-    pub fn initialize(script: &Script<'_>) -> Result<()> {
-        let func = if let Some(func) = &script.func {
-            func
-        } else {
-            "init"
-        };
-
-        println!("{:X?}", &script.aux_data);
-
-        let module = decode_module(Cursor::new(&script.script)).unwrap();
-        let mut store = init_store();
-        let module_instance = instantiate_module(&mut store, module, &[]).unwrap();
-        if let ExternVal::Func(main_addr) = get_export(&module_instance, func).unwrap() {
-            let res = invoke_func(
-                &mut store,
-                main_addr,
-                Vec::new(),
-                script.aux_data.as_ref(),
-                None,
-            );
-            println!("{:X?}", res);
-            save_store("some_txid", &store);
-            match res {
-                Ok(_) => Ok(()),
-                Err(_) => Err(VmErr::Unknown),
-            }
-        } else {
-            Err(VmErr::Unknown)
-        }
-    }
-
-    pub fn process_inbox(script: &Script<'_>, message: Option<Vec<u8>>) -> Result<()> {
-        let module = decode_module(Cursor::new(&script.script)).unwrap();
-        let mut store = init_store();
-        let func = if let Some(func) = script.func {
-            func
-        } else {
-            "inbox"
-        };
-        let module_instance = instantiate_module(&mut store, module, &[]).unwrap();
-        restore_store(&mut store, "some_txid");
-        if let ExternVal::Func(main_addr) = get_export(&module_instance, func).unwrap() {
-            let res = invoke_func(
-                &mut store,
-                main_addr,
-                Vec::new(),
-                script.aux_data.as_ref(),
-                message.as_ref(),
-            );
-            println!("{:X?}", res);
-            save_store("some_txid", &store);
-            match res {
-                Ok(_) => Ok(()),
-                Err(_) => Err(VmErr::Unknown),
-            }
-        } else {
-            Err(VmErr::Unknown)
-        }
-    }
+pub fn get_version() -> String {
+    env!("CARGO_PKG_VERSION").to_string()
 }
 
-impl CauchyVM for WasmVM {
-    fn initialize(script: &Script<'_>) -> Result<()> {
-        WasmVM::initialize(script)
-    }
+pub trait CauchyVM {
+    fn initialize(&mut self, script: &Script<'_>) -> Result<()>;
+    fn process_inbox(&mut self, script: &Script<'_>, message: Option<Vec<u8>>) -> Result<()>;
+}
 
-    fn process_inbox(script: &Script<'_>, message: Option<Vec<u8>>) -> Result<()> {
-        WasmVM::process_inbox(script, message)
+#[cfg(test)]
+mod test {
+    use crate::{CauchyVM, DefaultVM, Script};
+    #[test]
+    fn vm_interface() {
+        use std::io::prelude::*;
+
+        let dir = std::env::current_dir().unwrap().join(
+            "contracts/contract_data/target/wasm32-unknown-unknown/release/contract_data.wasm",
+        );
+        let mut f = std::fs::File::open(dir.as_path()).expect("failed to open contract_data.wasm");
+        let mut script = Vec::new();
+        f.read_to_end(&mut script)
+            .expect("failed to read contract_data.wasm");
+        let aux_data = Some(vec![0x41, 0x42, 0x43, 0x44, 0x45]);
+        let script = Script {
+            func: None,
+            script,
+            aux_data,
+        };
+
+        let mut vm = DefaultVM::default();
+        vm.initialize(&script).unwrap();
+        vm.process_inbox(&script, None).unwrap();
     }
 }
 
